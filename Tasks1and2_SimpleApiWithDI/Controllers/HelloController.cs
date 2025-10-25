5ad3033ed1fe9db7b512ea3fdbc389c067461d93
Kirill Zhestkov <infinity.crime@outlook.com>
2025-10-25 14:54:30 +0300

    TASK-307-1: Добавлен контроллер, который возвращает object с полем string (обычное сообщение) в формате JSON.

---
+++ Tasks1and2_SimpleApiWithDI/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tasks1and2_SimpleApiWithDI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            // чтобы вернуть сообщение в JSON, нам нужно вернуть именно объект, а не просто строку
+            return Ok(new {message = "Hello! It is HelloConroller!"});
+        }
+    }
+}
