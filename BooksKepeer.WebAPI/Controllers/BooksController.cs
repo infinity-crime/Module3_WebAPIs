1e375afda069feeda9ea09a6fd128c4c36dba6cd
Kirill Zhestkov <infinity.crime@outlook.com>
2025-10-26 20:24:14 +0300

    TASK-307-9: Настроен и подключен Swagger вместе с XML комментариями к методам контроллера.

--- BooksKepeer.WebAPI/Controllers/BooksController.cs
+++ BooksKepeer.WebAPI/Controllers/BooksController.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Options;
 
 namespace BooksKepeer.WebAPI.Controllers
 {
+    /// <summary>
+    /// API для управления списком книг
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class BooksController : BaseController
@@ -23,18 +26,31 @@ namespace BooksKepeer.WebAPI.Controllers
             _apiSettings = apiOptions.Value;
         }
 
+        /// <summary>
+        /// Получение текущих настроек API
+        /// </summary>
+        /// <returns>Объект, хранящий настройки</returns>
         [HttpGet("api-info")]
         public IActionResult GetApiInfo()
         {
             return Ok(_apiSettings);
         }
 
+        /// <summary>
+        /// Получение списка книг из памяти (может быть пустым)
+        /// </summary>
+        /// <returns>Список книг</returns>
         [HttpGet("all-books")]
         public async Task<IActionResult> GetAllBooks()
         {
             return Ok(await _bookService.GetAll());
         }
 
+        /// <summary>
+        /// Получение выбранной книги по ее ID
+        /// </summary>
+        /// <param name="id">ID книги типа Guid</param>
+        /// <returns>DTO выбранной книги</returns>
         [HttpGet("{id}")]
         public async Task<IActionResult> GetBook([FromRoute] Guid id)
         {
@@ -43,6 +59,11 @@ namespace BooksKepeer.WebAPI.Controllers
             return HandleResult<BookDto>(result);
         }
 
+        /// <summary>
+        /// Создание книги в списке
+        /// </summary>
+        /// <param name="request">Запрос (DTO) с данными для создания</param>
+        /// <returns>Созданная книга</returns>
         [HttpPost]
         public async Task<IActionResult> CreateBook([FromBody] CreateBookRequest request)
         {
@@ -51,6 +72,12 @@ namespace BooksKepeer.WebAPI.Controllers
             return HandleResult<BookDto>(result);
         }
 
+        /// <summary>
+        /// Обновление выбранной книги по ее ID
+        /// </summary>
+        /// <param name="id">ID книги типа Guid</param>
+        /// <param name="request">Запрос (DTO) с данными для обновления</param>
+        /// <returns>Status code</returns>
         [HttpPut("update/{id}")]
         public async Task<IActionResult> UpdateBook([FromRoute] Guid id, [FromBody] UpdateBookRequest request)
         {
@@ -59,6 +86,11 @@ namespace BooksKepeer.WebAPI.Controllers
             return HandleResult(result);
         }
 
+        /// <summary>
+        /// Удаление выбранной книги по ее ID
+        /// </summary>
+        /// <param name="id">ID книги типа Guid</param>
+        /// <returns>Status code</returns>
         [HttpDelete("delete/{id}")]
         public async Task<IActionResult> DeleteBook([FromRoute] Guid id)
         {
--- BooksKepeer.WebAPI/Program.cs
+++ BooksKepeer.WebAPI/Program.cs
@@ -4,6 +4,8 @@ using BooksKepeer.WebAPI.Middleware;
 using BooksKepeer.WebAPI.Settings;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Swashbuckle.AspNetCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,8 +20,40 @@ builder.Services.AddControllers();
 // Читаем нашу конфигурацию из appsettings.json
 builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings"));
 
+// настройка документации Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Books Keeper API",
+        Version = "v1",
+        Description = "Simple REST-API made as an internship in Techcore.",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Kirill Zhestkov",
+            Email = "kirillzhestkov78@gmail.com"
+        }
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+
+    if(File.Exists(xmlPath))
+    {
+        o.IncludeXmlComments(xmlPath);
+    }
+});
+
 
 var app = builder.Build();
+
+// Подключаем непосредственно настроенный Swagger
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Использование кастомного middleware для отлова ошибок
 app.UseMiddleware<ExceptionHandlerMiddleware>();
