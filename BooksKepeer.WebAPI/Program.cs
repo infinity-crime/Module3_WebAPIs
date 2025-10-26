458af422b7a17efe62f0b8d24108dedf4784ac83
Kirill Zhestkov <infinity.crime@outlook.com>
2025-10-26 18:52:16 +0300

    TASK-307-6: Добавлена валидация POST запросов с телом (FluentValidation).

--- BooksKepeer.WebAPI/Program.cs
+++ BooksKepeer.WebAPI/Program.cs
@@ -1,19 +1,26 @@
 using BooksKeeper.Application.Interfaces;
 using BooksKeeper.Application.Services;
 using BooksKepeer.WebAPI.Middleware;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IBookService, BookService>();
 
+// Регистрация FluentValidation
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
 builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+// Использование кастомного middleware для отлова ошибок
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 
+// Использование кастомного middleware для замера выполнения запросов
 app.UseMiddleware<RequestTimingMiddleware>();
 
 app.UseHttpsRedirection();
