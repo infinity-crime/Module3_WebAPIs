918ced4dc67067e6ae396b3e454b21551af9fd37
Kirill Zhestkov <infinity.crime@outlook.com>
2025-11-05 23:11:11 +0300

    TASK-315-4: написан тест для DeleteAsync() и проверен вызов метода у репозитория.

--- tests/BooksKeeper.Tests/BookServiceTests.cs
+++ tests/BooksKeeper.Tests/BookServiceTests.cs
@@ -8,6 +8,7 @@ using BooksKeeper.Application.Interfaces;
 using BooksKeeper.Application.Services;
 using BooksKeeper.Domain.Entities;
 using BooksKeeper.Domain.Interfaces;
+using BooksKeeper.Domain.Interfaces.Common;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Moq;
 
@@ -45,6 +46,43 @@ namespace BooksKeeper.Tests
             Assert.NotNull(result.Value);
             Assert.Equal(expectedBook.Id, result.Value.Id);
             Assert.Equal(expectedBook.Title, result.Value.Title);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_BookExists_CallDeleteEntityAsync()
+        {
+            // Arrange
+            var bookRepositoryMock = new Mock<IBookRepository>();
+            var cacheServiceMock = new Mock<ICacheService>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var expectedBook = Book.Create("Test Book", 2023);
+
+            bookRepositoryMock
+                .Setup(r => r.GetByIdAsync(expectedBook.Id, true, true))
+                .ReturnsAsync(expectedBook);
+
+            // Настройка мока для метода DeleteAsync не требуется, так как он ничего не возвращает (но в названии есть Async, оно неверное, т.е. метод синхронный)
+
+            unitOfWorkMock
+                .Setup(u => u.SaveChangesAsync())
+                .Returns(Task.CompletedTask);
+
+            cacheServiceMock
+                .Setup(c => c.RemoveAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+
+            var service = new BookService(bookRepositoryMock.Object, null!, unitOfWorkMock.Object, null!, cacheServiceMock.Object);
+
+            // Act
+            var result = await service.DeleteByIdAsync(expectedBook.Id);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+
+            // Verify вызов метода DeleteAsync с правильным объектом Book и SaveChangesAsync
+            bookRepositoryMock.Verify(r => r.DeleteAsync(It.Is<Book>(b => b.Id == expectedBook.Id)), Times.Once);
+            unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
         }
     }
 }
