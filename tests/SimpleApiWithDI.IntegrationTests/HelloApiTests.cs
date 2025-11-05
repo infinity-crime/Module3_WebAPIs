6e559b4caba972f15af2ef4f71c5e8a28e9abe95
Kirill Zhestkov <infinity.crime@outlook.com>
2025-11-05 23:40:54 +0300

    TASK-315-5: добавлен интеграционный тест для простого проекта WebAPI.

---
+++ tests/SimpleApiWithDI.IntegrationTests/HelloApiTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace SimpleApiWithDI.IntegrationTests
+{
+    public class HelloApiTests
+    {
+        [Fact]
+        public async Task Get_HelloEndpoint_ReturnsOk()
+        {
+            // Arrange
+            using var application = new WebApplicationFactory<Program>();
+            var client = application.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("api/hello");
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
