e62ddd08e4ce27d1367d96d06b0b9d1a891eeabb
Kirill Zhestkov <infinity.crime@outlook.com>
2025-11-06 01:48:47 +0300

    TASK-315-6: Добавлен интеграционный тест получения всех авторов из In-Memory Db.

---
+++ tests/BooksKeeper.IntegrationInMemoryDbTests/AuthorControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using BooksKeeper.Application.DTOs.Responses;
+using BooksKeeper.Infrastructure.Data;
+using BooksKeeper.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Text.Json;
+
+namespace BooksKeeper.IntegrationInMemoryDbTests
+{
+    public class AuthorControllerTests
+    {
+        [Fact]
+        public async Task GetAllAuthors_ReturnsAuthors()
+        {
+            // Arrange
+            var factory = new MyTestFactory();
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("api/authors/all-authors");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ tests/BooksKeeper.IntegrationInMemoryDbTests/MyTestFactory.cs
@@ -0,0 +1,64 @@
+﻿using BooksKeeper.Domain.Entities;
+using BooksKeeper.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksKeeper.IntegrationInMemoryDbTests
+{
+    public class MyTestFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Удаляем существующие регистрации DbContext
+                RemoveAllDbContextRegistrations(services);
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDB");
+                });
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+
+                // Создаем новую БД для каждого теста
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                // Сеим тестовые данные
+                SeedTestData(db);
+            });
+        }
+
+        private static void RemoveAllDbContextRegistrations(IServiceCollection services)
+        {
+            var descriptors = services.Where(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>) ||
+                                                  d.ServiceType == typeof(DbContextOptions) ||
+                                                  d.ServiceType.Name.Contains("DbContext") ||
+                                                  d.ServiceType == typeof(ApplicationDbContext)).ToList();
+            foreach (var descriptor in descriptors)
+            {
+                services.Remove(descriptor);
+            }
+        }
+
+        private static void SeedTestData(ApplicationDbContext context)
+        {
+            context.Authors.Add(Author.Create("Jhon", "Mister"));
+            context.Authors.Add(Author.Create("Victoria", "Secret"));
+            context.SaveChanges();
+        }
+    }
+}
