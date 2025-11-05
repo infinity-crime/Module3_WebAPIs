a2a6b4521e352a6697ce586295d1c5041ea47e01
Kirill Zhestkov <infinity.crime@outlook.com>
2025-11-05 21:13:14 +0300

    TASK-315-1: В папку tests добавлен мини проект для калькулятора, который умеет складывать два числа. Также добавлен проект XUnit специально для этого калькулятора. Написан тест без параметров, при проверке - зеленый.

---
+++ tests/LibCalculator.Tests/LibCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using LibCalculator;
+using Xunit;
+
+namespace LibCalculator.Tests
+{
+    public class LibCalculatorTests
+    {
+        [Fact]
+        public void Add_TwoPositiveNumbers_ReturnsSum()
+        {
+            // Arange
+            int a = 2;
+            int b = 3;
+
+            // Act
+            int result = AddCalculator.Add(a, b);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
