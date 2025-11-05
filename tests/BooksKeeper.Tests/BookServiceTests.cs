59dd42089e9fd073bc6349e139e95bc47b1a5f4f
Kirill Zhestkov <infinity.crime@outlook.com>
2025-11-05 22:46:11 +0300

    TASK-315-3: написан тест для сервиса BookService,  который мокает репозиторий для книг, а также кэш сервис для Redis, так как в сервисе идет первоначально вызов именно его.

---
+++ tests/BooksKeeper.Tests/BookServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksKeeper.Application.DTOs.Responses;
+using BooksKeeper.Application.Interfaces;
+using BooksKeeper.Application.Services;
+using BooksKeeper.Domain.Entities;
+using BooksKeeper.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Moq;
+
+namespace BooksKeeper.Tests
+{
+    public class BookServiceTests
+    {
+        [Fact]
+        public async Task GetByIdAsync_BookExists_ReturnsBook()
+        {
+            // Arrange
+            var bookRepositoryMock = new Mock<IBookRepository>();
+            var cacheServiceMock = new Mock<ICacheService>();
+
+            // Подготовка мок-объекта для метода GetByIdAsync
+            var expectedBook = Book.Create("Test Book", 2023);
+
+            // Настройка мока для возврата ожидаемой книги
+            bookRepositoryMock
+                .Setup(r => r.GetByIdAsync(expectedBook.Id, It.IsAny<bool>(), It.IsAny<bool>()))
+                .ReturnsAsync(expectedBook);
+
+            // Настройка мока для возврата null из кэша
+            cacheServiceMock
+                .Setup(c => c.GetAsync<BookResponse>(It.IsAny<string>()))
+                .ReturnsAsync((BookResponse?)null);
+
+            // Создание экземпляра BookService с использованием мок-объекта
+            var service = new BookService(bookRepositoryMock.Object, null!, null!, null!, cacheServiceMock.Object);
+
+            // Act
+            var result = await service.GetByIdAsync(expectedBook.Id);
+
+            // Assert - проверка результата
+            Assert.NotNull(result.Value);
+            Assert.Equal(expectedBook.Id, result.Value.Id);
+            Assert.Equal(expectedBook.Title, result.Value.Title);
+        }
+    }
+}
