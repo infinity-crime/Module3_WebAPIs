f49c50a21bb9fd9809d2e6ea9c3f78e7648c14bc
Kirill Zhestkov <infinity.crime@outlook.com>
2025-11-09 17:51:40 +0300

    TASK-319-4: Создан контроллер с POST методом, который публикует сообщение в exchange (метод publish(command)).

---
+++ BooksKepeer.WebAPI/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Orders.Contracts.Commands;
+
+namespace BooksKepeer.Books.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IBus _bus;
+
+        public OrdersController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost("order")]
+        public async Task<IActionResult> PublishOrderMessage([FromBody] SubmitOrderCommand command)
+        {
+            await _bus.Publish(command);
+
+            return Accepted();
+        }
+    }
+}
